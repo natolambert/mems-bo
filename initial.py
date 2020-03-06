6709e683e15c699f9fbc4108f9a836564ec290d1
Nathan Lambert <nol@fb.com>
2020-03-05 19:36:49 -0800

    formatting experiment

--- initial.py
+++ initial.py
@@ -7,6 +7,8 @@ from gpytorch.mlls import ExactMarginalLogLikelihood
 
 import pandas as pd
 
+init_data = pd.read_csv('data/Valves_v0.csv')
+constraints = pd.read_csv('data/Valves_constraints.csv')
 from ax import optimize
 # best_parameters, best_values, experiment, model = optimize(
 #         parameters=[
@@ -26,7 +28,67 @@ from ax import optimize
 #         minimize=True,
 #     )
 
+from ax.modelbridge.registry import Models
 from ax.service.ax_client import AxClient
+
+opt_list = ["N (fingers #)", "a (um)", ]
+data_sub = init_data[1]
+
+# TODO
+# sub data, add elements to experiment
+# figure out how the data is matched to a objective in a reasonable way
+# sample new values / save
+# visualization 
+def gen_parameters(data, constraints, opt_list):
+    parameters = []
+    for o in opt_list:
+        sub = dict()
+        typ = constraints[o][2]
+        sub["name"] = o
+        sub["type"] = "range"
+        if typ == "float":
+            sub["bounds"] = [float(constraints[o][0]), float(constraints[o][1])]
+        elif typ == "int":
+            sub["bounds"] = [int(constraints[o][0]), int(constraints[o][1])]
+        else:
+            raise ValueError("Type not supported")
+        sub["value_type"] = typ
+        sub["log_scale"] = False
+        parameters.append(sub)
+    return parameters
+
+
+parameters = gen_parameters(init_data, constraints, opt_list)
+
+ax_client = AxClient()
+ax_client.create_experiment(
+    name="Thermo-mechnaical Valve",
+    parameters=parameters,
+    objective_name="hartmann6",
+    minimize=True,  # Optional, defaults to False.
+    parameter_constraints=None,  # Optional.
+    outcome_constraints=None,  # Optional.
+)
+
+# Pull out of API for more specific iteration
+exp = ax_client._experiment
+batch = 10
+print(f"Running Batch GP+EI optimization of {batch} samples")
+# Reinitialize GP+EI model at each step with updated data.
+gpei = Models.BOTORCH(experiment=exp, data=init_data)
+generator = gpei.gen(5)
+# generator_run = gpei.gen(5)
+# experiment.new_batch_trial(generator_run=generator_run)
+
+
+quit()
+sobol = Models.SOBOL(search_space=experiment.search_space)
+generator_run = sobol.gen(5)
+
+batch = exp.new_trial(generator_run=gpei.gen(1))
+
+quit()
+
 ax_client = AxClient()
 ax_client.create_experiment(
     name="hartmann_test_experiment",
@@ -92,11 +154,9 @@ best_parameters, values = ax_client.get_best_parameters()
 
 from ax.utils.notebook.plotting import render, init_notebook_plotting
 
-
 render(ax_client.get_contour_plot())
 render(ax_client.get_contour_plot(param_x="x3", param_y="x4", metric_name="l2norm"))
 render(ax_client.get_optimization_trace(objective_optimum=hartmann6.fmin))  # Objective_optimum is optional.
 
 ax_client.save_to_json_file()  # For custom filepath, pass `filepath` argument.
 restored_ax_client = AxClient.load_from_json_file()  # For custom filepath, pass `filepath` argument.
-
